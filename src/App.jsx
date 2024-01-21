5ede449f079f754326ad1d492b272d0dced47460
Arif Dhali <arifdhali98@gmail.com>
2024-01-21 12:04:41 +0530

    quize completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 const App = () => {
   const [question, setQuestion] = useState(0);
   const [score, setScore] = useState(0);
+  const [quizCompleted, setQuizCompleted] = useState(false);
   const currentQuestion = Data[question];
   const totalQuestions = Data.length;
 
@@ -15,12 +16,12 @@ const App = () => {
     if (question + 1 < totalQuestions) {
       setQuestion((oldQuestion) => oldQuestion + 1);
     } else {
+      setQuizCompleted(true);
       console.log("Quiz completed!");
     }
   };
 
   const statusLineWidth = ((question + 1) / totalQuestions) * 100;
-
   return (
     <>
       <div className="container">
@@ -42,26 +43,38 @@ const App = () => {
               <h2 className="text-red-700">{score}</h2>
             </div>
           </div>
+
           <div className="quize-body mt-8 text-left">
-            <div className="quize-question">
-              <h1 className="text-xl font-semibold ">
-                {question + 1}. {currentQuestion.questionTxt}
-              </h1>
-            </div>
-            <ul className="quize-answer-list pt-8">
-              {currentQuestion.answerOptions.map((item, index) => (
-                <li
-                  key={index}
-                  className="flex items-center cursor-pointer shadow-md hover:bg-green-100"
-                  onClick={() => handleCorrectQuestion(item.isCorrect)}
-                >
-                  <div className="counter py-2 px-5 bg-rose-400 mr-4 text-white ">
-                    {index + 1}
-                  </div>
-                  <p>{item.answerText}</p>
-                </li>
-              ))}
-            </ul>
+            {quizCompleted != true ? (
+              <>
+                <div className="quize-question">
+                  <h1 className="text-xl font-semibold ">
+                    {question + 1}. {currentQuestion.questionTxt}
+                  </h1>
+                </div>
+                <ul className="quize-answer-list pt-8">
+                  {currentQuestion.answerOptions.map((item, index) => (
+                    <li
+                      key={index}
+                      className="flex items-center cursor-pointer shadow-md hover:bg-green-100"
+                      onClick={() => handleCorrectQuestion(item.isCorrect)}
+                    >
+                      <div className="counter py-2 px-5 bg-rose-400 mr-4 text-white ">
+                        {index + 1}
+                      </div>
+                      <p>{item.answerText}</p>
+                    </li>
+                  ))}
+                </ul>
+              </>
+            ) : (
+              <>
+                <strong className="font-semibold text-xl text-red-600">
+                  Game over !
+                </strong>
+                <h2>Thank you for playing...</h2>
+              </>
+            )}
           </div>
         </div>
       </div>
